ad4ad821dfb7bb63a58951e9941ea06925db983f
Eric <ericxgao@gmail.com>
2018-01-10 16:59:32 -0800

    New

---
+++ dump_names.py
@@ -0,0 +1,13 @@
+import json
+
+
+def print_data(infile):
+	data = json.load(infile)
+	for key in data:
+		print('Name: %s - Website: %s' % (data[key]['name'], data[key]['website']))
+		
+with open('active.json') as infile:
+   	print_data(infile)
+
+with open('upcoming.json') as infile:
+    print_data(infile)
